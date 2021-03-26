303bace468acf37bbd379b1fbf8b84d32d81eafc
Jack Lin <jackharper775@gmail.com>
2021-03-26 11:09:16 +0800

    first commit

---
+++ read.py
@@ -0,0 +1,66 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 10000 == 0:
+			print(len(data))
+print('檔案讀取完了,總共有', len(data),'筆資料')
+
+wc = {} #word count
+for d in data:
+	words = d.split(' ')
+	for word in words:
+		if word in wc:
+			wc[word] += 1 
+		else:
+			wc[word] = 1 #新增新的key進wc 字典
+
+for word in wc:
+	if wc[word] > 100000:
+		print(word, wc[word])
+
+print(len(wc))
+print(wc['Jack'])
+
+while True:
+	word = input('請問你想查什麼字:')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word,'出現過的字數為:', wc[word])
+	else:
+		print('這個字沒有出現過喔')
+		
+print('感謝使用本查詢功能')
+
+# sum_len = 0 
+# for d in data:
+# 	sum_len = sum_len + len(d)
+# print ('留言的平均長度為', sum_len/len(data))	
+
+# new = []
+# for d in data:
+# 	if len(d) < 100:
+# 		new.append(d)
+
+# print('一共有', len(new), '筆留言長度小於100')
+# print(new[0])
+
+# good = []
+# for d in data:
+# 	if 'good' in d:
+# 		good.append(d)
+# print('一共有', len(good), '筆留言提到Good')
+
+
+# #清單快寫法
+# good = [d for d in data if 'food' in d]
+
+
+# y = 0
+# for d in data:
+# 	if 'good' in d:
+# 		y = y+1
+# print(y)
